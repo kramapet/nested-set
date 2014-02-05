4791135f7255ecfdc51b4e42351afabb67ea97ac
Petr Kramar <kramar@masquerade.cz>
2014-02-05 14:57:38 +0100

    comment out lines for inserting sample data

--- sql/dump.sql
+++ sql/dump.sql
@@ -7,11 +7,11 @@ create table `tree` (
 	primary key (`name`)
 );
 
-insert into `tree` values ('Alpha', '', 1, 16);
-insert into `tree` values ('Beta', 'Alpha', 2, 9);
-insert into `tree` values ('Gamma', 'Alpha', 10, 15);
-insert into `tree` values ('Delta', 'Beta', 3, 4);
-insert into `tree` values ('Epsilon', 'Beta', 5, 6);
-insert into `tree` values ('Zeta', 'Beta', 7, 8);
-insert into `tree` values ('Eta', 'Gamma', 11, 12);
-insert into `tree` values ('Theta', 'Gamma', 13, 14);
+-- insert into `tree` values ('Alpha', '', 1, 16);
+-- insert into `tree` values ('Beta', 'Alpha', 2, 9);
+-- insert into `tree` values ('Gamma', 'Alpha', 10, 15);
+-- insert into `tree` values ('Delta', 'Beta', 3, 4);
+-- insert into `tree` values ('Epsilon', 'Beta', 5, 6);
+-- insert into `tree` values ('Zeta', 'Beta', 7, 8);
+-- insert into `tree` values ('Eta', 'Gamma', 11, 12);
+-- insert into `tree` values ('Theta', 'Gamma', 13, 14);
