e7c65b2d54c4f41f94c485e525bff07751ed2164
Petr Kramar <kramar@masquerade.cz>
2014-02-05 14:57:07 +0100

    add tests for 'NestedTree\TreeItem'

---
+++ tests/NestedTree/TreeItemTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * NestedTree\TreeItem test
+ */
+
+use \NestedTree\TreeItem;
+
+class TreeItemTest extends \PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+	}
+
+	public function testIsLeaf() {
+		$item = new TreeItem();
+		$this->assertTrue($item->isLeaf());
+
+		$item2 = new TreeItem($item);
+		$this->assertFalse($item->isLeaf());
+	}
+
+	public function testGetData() {
+		$item = new TreeItem();
+		$this->assertEquals($item->getData(), NULL);
+
+		$d = array('name' => 'Hello', 'lft' => 1, 'rgt' => 16);
+		$item2 = new TreeItem(NULL, $d);
+		$this->assertEquals($item2->getData(), $d);
+
+	}
+
+	public function testGetParent() {
+		$parent = new TreeItem();
+		$this->assertEquals($parent->getParent(), NULL);
+
+		$child = new TreeItem($parent);
+		$this->assertEquals($child->getParent(), $parent);
+	}
+
+	public function testSetParent() {
+		$parent = new TreeItem();
+		$child = new TreeItem();
+		$child->setParent($parent);
+
+		$this->assertEquals($child->getParent(), $parent);
+	}
+}
